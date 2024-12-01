a91e751852239455f3d18aa08e9357031763c4b6
tenorioarandas <tenorio.arandas@gmail.com>
2024-12-01 18:51:26 -0300

    :sparkles: feat: Criação da página Quero Doar

--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,7 +1,27 @@
+import iconeLivro from "../../assets/iconeLivro.png";
+import s from "../queroDoar/querodoar.module.scss";
+
 export default function QueroDoar() {
   return (
-    <section>
-      <h2>Eu sou a Quero Doar</h2>
+    <section className={s.section}>
+      <h2>
+        Por favor, preencha o formulário com suas informações e as informações
+        do Livro
+      </h2>
+      <form className={s.form}>
+        <img src={iconeLivro} alt="Ícone de um livro azul aberto" />
+        <h2>Informações do Livro</h2>
+
+        <input type="text" id="" name="" placeholder="Título" />
+
+        <input type="text" id="" name="" placeholder="Categoria" />
+
+        <input type="text" id="" name="" placeholder="Autor(a)" />
+
+        <input type="text" id="" name="" placeholder="Imagem" />
+
+        <button type="submit">Doar</button>
+      </form>
     </section>
   );
 }
