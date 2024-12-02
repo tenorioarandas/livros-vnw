e31d8b6cc47aabb6de687e42b5bfd50c2024b1aa
tenorioarandas <tenorio.arandas@gmail.com>
2024-12-01 21:05:50 -0300

    :sparkles: feat: Criação do Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 import "./GlobalStyle/globalStyle.scss";
 
 export default function App() {
   return (
     <section>
       <Header />
+      <Footer />
     </section>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,26 @@
+import facebook from "../../assets/facebookIcon.png";
+import twitter from "../../assets/twitterIcon.png";
+import youtube from "../../assets/youtubeIcon.png";
+import linkedin from "../../assets/linkedinIcon.png";
+import instagram from "../../assets/instagramIcon.png";
+import s from "../footer/footer.module.scss";
+
 export default function Footer() {
   return (
-    <footer>
-      <h2>Footer!</h2>
-    </footer>
+    <>
+      <footer className={s.footer}>
+        <p>4002-8922</p>
+        <div className={s.imgContainer}>
+          <img src={facebook} alt="" />
+          <img src={twitter} alt="" />
+          <img src={youtube} alt="" />
+          <img src={linkedin} alt="" />
+          <img src={instagram} alt="" />
+        </div>
+      </footer>
+      <div className={s.creditosBox}>
+        <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024</p>
+      </div>
+    </>
   );
 }
